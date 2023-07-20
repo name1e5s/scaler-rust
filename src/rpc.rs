998df206b339bd8e3de2c5951480e09922545049
name1e5s <me@hai-hs.in>
2023-07-20 23:52:55 +0800

    Dummp impl

---
+++ src/rpc.rs
@@ -0,0 +1,1 @@
+tonic::include_proto!("serverless.simulator");
---
+++ src/server/mod.rs
@@ -0,0 +1,23 @@
+pub mod platform;
+
+use crate::rpc::scaler_server::Scaler;
+use crate::rpc::{AssignReply, AssignRequest, IdleReply, IdleRequest};
+use tonic::{Request, Response, Status};
+
+type TonicResult<T> = std::result::Result<Response<T>, Status>;
+
+pub struct ScalerImpl;
+
+#[tonic::async_trait]
+impl Scaler for ScalerImpl {
+    async fn assign(&self, _: Request<AssignRequest>) -> TonicResult<AssignReply> {
+        Ok(Response::new(AssignReply {
+            ..Default::default()
+        }))
+    }
+    async fn idle(&self, _: Request<IdleRequest>) -> TonicResult<IdleReply> {
+        Ok(Response::new(IdleReply {
+            ..Default::default()
+        }))
+    }
+}
