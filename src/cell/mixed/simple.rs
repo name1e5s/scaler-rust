a596105ac1c21badba251b16e72cd13d12ba51c8
name1e5s <me@hai-hs.in>
2023-08-05 10:29:48 +0800

    Use different impl for nodes and csinodes

--- src/cell/mixed/simple.rs
+++ src/cell/mixed/simple.rs
@@ -1,7 +1,10 @@
 use std::sync::Arc;
 
 use crate::{
-    cell::{mixed::MixedCell, MemoryCellFactory, NaiveSet1CellFactory, NaiveSet2CellFactory},
+    cell::{
+        mixed::MixedCell, DirectCellFactory, MemoryCellFactory, NaiveCellFactory,
+        NaiveSet1CellFactory, NaiveSet2CellFactory,
+    },
     model,
     platform::Platform,
     scheduler::cell::{Cell, CellFactory},
@@ -13,6 +16,10 @@ impl CellFactory<MixedCell> for SimpleMixedCellFactory {
     fn new(&self, meta: model::Meta, client: Arc<Platform>) -> Arc<MixedCell> {
         if meta.key.len() >= 30 {
             MixedCell::new(MemoryCellFactory.new(meta, client) as Arc<dyn Cell>)
+        } else if meta.key.starts_with("nodes") {
+            MixedCell::new(NaiveCellFactory.new(meta, client) as Arc<dyn Cell>)
+        } else if meta.key.starts_with("csinodes") {
+            MixedCell::new(DirectCellFactory.new(meta, client) as Arc<dyn Cell>)
         } else if meta.key.ends_with("1") {
             MixedCell::new(NaiveSet1CellFactory.new(meta, client) as Arc<dyn Cell>)
         } else {
--- src/cell/mod.rs
+++ src/cell/mod.rs
@@ -1,6 +1,7 @@
 mod base;
 mod memory;
 mod metric;
+mod naive;
 mod naive_set_1;
 mod naive_set_2;
 mod stork;
@@ -10,5 +11,73 @@ pub mod mixed;
 pub use base::BaseCell;
 pub use memory::{MemoryCell, MemoryCellFactory};
 pub use metric::CellMetric;
+pub use naive::{NaiveCell, NaiveCellFactory};
 pub use naive_set_1::{NaiveSet1Cell, NaiveSet1CellFactory};
 pub use naive_set_2::{NaiveSet2Cell, NaiveSet2CellFactory};
+
+use crate::{
+    model,
+    platform::Platform,
+    scheduler::cell::{Cell, CellFactory},
+};
+use anyhow::Result;
+use dashmap::DashMap;
+use std::sync::Arc;
+
+pub struct DirectCell {
+    meta: model::Meta,
+    client: Arc<Platform>,
+    // request_id -> instance_id
+    map: DashMap<String, model::Slot>,
+}
+
+impl DirectCell {
+    fn new(meta: model::Meta, client: Arc<Platform>) -> DirectCell {
+        DirectCell {
+            meta,
+            client,
+            map: DashMap::new(),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl Cell for DirectCell {
+    async fn assign(self: Arc<Self>, request_id: String, _: u64) -> Result<model::Assignment> {
+        let meta_key = self.meta.key.clone();
+        let slot = self
+            .client
+            .create_slot(
+                model::ResourceConfig {
+                    memory_in_megabytes: self.meta.memory_in_mb,
+                },
+                self.meta.clone(),
+            )
+            .await?;
+        self.map.insert(request_id.clone(), slot.clone());
+        Ok(model::Assignment {
+            request_id,
+            meta_key,
+            instance_id: slot.instance_id,
+        })
+    }
+    async fn idle(
+        self: Arc<Self>,
+        assignment: model::Assignment,
+        _: model::IdleReason,
+    ) -> Result<()> {
+        let slot = self.map.get(&assignment.request_id).unwrap().clone();
+        self.client
+            .destroy_slot(&slot.instance_id, &slot.id)
+            .await?;
+        Ok(())
+    }
+}
+
+pub struct DirectCellFactory;
+
+impl CellFactory<DirectCell> for DirectCellFactory {
+    fn new(&self, meta: model::Meta, client: Arc<Platform>) -> Arc<DirectCell> {
+        Arc::new(DirectCell::new(meta, client))
+    }
+}
